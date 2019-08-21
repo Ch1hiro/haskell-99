6f35deb6f5af90df1c020957f0fbdc3b54c2fafe
sano <tekitou@tekitou.com>
2019-08-21 19:37:56 +0900

    練習問題02まで

---
+++ h99.hs
@@ -0,0 +1,13 @@
+myLast :: [a] -> a
+-- erroに対する例外処理がない, のでそれを追加
+myLast [] = error "no end for empty lists!"
+-- これは別の書き方もできる
+-- myLast [x] = x
+myLast (x:[]) = x
+myLast (_:xs) = myLast xs
+
+myButLast :: [a] -> a
+myButLast [] = error "no end for empty lists!"
+myButLast [x] = error "ちょっとこれは無理"
+myButLast (x:y:[]) = x
+myButLast (_:xs) = myButLast xs
