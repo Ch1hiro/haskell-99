749fae530985bde09f56b391b9d7608dd4f5c302
Sano Chihiro <sanochhiro.art@gmail.com>
2021-09-15 16:54:34 +0900

    solved q3

--- h99_01-10.hs
+++ h99_01-10.hs
@@ -10,3 +10,15 @@ myButLast []     = error "myButLast needs more than 1 elements!!"
 myButLast [x]    = error "myButLast needs more than 1 elements!!"
 myButLast (x:y:[])  = x
 myButLast (_:xs) = myButLast xs
+
+--question03,  Sep 15th 2021
+elementAt :: [a] -> Int -> a
+elementAt []     _ = error "no such elemet"
+elementAt (x:_)  1 = x
+elementAt (_:xs) k =
+      if (k<2)
+            then error "k value should be positive Int"
+      else
+            elementAt xs (k-1)
+
+--question04, Sep 15th 2021
