c885f31799b4955ae87163ef477e7ccf478a3c6f
Sano Chihiro <sanochhiro.art@gmail.com>
2021-09-15 16:31:42 +0900

    suggest q2

--- h99_01-10.hs
+++ h99_01-10.hs
@@ -2,3 +2,11 @@
 myLast :: [a] -> a
 myLast (x:[]) = x
 myLast (x:xs) = myLast xs
+
+
+--question02, Sep 15th 2021
+myButLast :: [a] -> a
+myButLast []     = error "myButLast needs more than 1 elements!!"
+myButLast [x]    = error "myButLast needs more than 1 elements!!"
+myButLast [x:_]  = x
+myButLast [_:xs] = myButLast xs
