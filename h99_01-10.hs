65d13b0b192b47b162a9de13d7d509a57633a2c2
Sano Chihiro <sanochhiro.art@gmail.com>
2021-09-15 17:16:42 +0900

    overCome q4

--- h99_01-10.hs
+++ h99_01-10.hs
@@ -6,10 +6,10 @@ myLast (x:xs) = myLast xs
 
 --question02, Sep 15th 2021
 myButLast :: [a] -> a
-myButLast []     = error "myButLast needs more than 1 elements!!"
-myButLast [x]    = error "myButLast needs more than 1 elements!!"
+myButLast []        = error "myButLast needs more than 1 elements!!"
+myButLast [x]       = error "myButLast needs more than 1 elements!!"
 myButLast (x:y:[])  = x
-myButLast (_:xs) = myButLast xs
+myButLast (_:xs)    = myButLast xs
 
 --question03,  Sep 15th 2021
 elementAt :: [a] -> Int -> a
@@ -23,5 +23,5 @@ elementAt (_:xs) k =
 
 --question04, Sep 15th 2021
 myLength :: [a] -> Int
-myLength [] = 0
-myLength _:xs = 1 + myLength xs
+myLength []     = 0
+myLength (_:xs) = 1 + (myLength xs)
