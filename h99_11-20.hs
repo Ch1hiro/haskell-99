0c85a7026c113d5f4ec40130ace4996ac704655c
Sano Chihiro <sanochhiro.art@gmail.com>
2021-09-20 01:18:16 +0900

    Suggest conpletly working P12

--- h99_11-20.hs
+++ h99_11-20.hs
@@ -15,3 +15,25 @@ encodeModified_pre x
 
 encodeModified :: (Show a, Eq a) => [a] -> [String]
 encodeModified　x = map encodeModified_pre (encode x)
+
+
+--problem 12
+{--
+Prelude> takeWhile (/= ' ') "Multiple 4 2"
+"Multiple"
+
+take read 2つめ
+
+repeat 3つめ
+
+--}
+
+import Data.Char
+
+decodeHelper :: String -> String
+decodeHelper x
+      |(takeWhile (/= ' ') x) == "Multiple"     = take (digitToInt $ head $ tail $ dropWhile (/= ' ') x) (repeat ((reverse x) !! 1))
+      |(takeWhile (/= ' ') x) == "Single"       = ((reverse x) !! 1) : []s
+
+decodeModified :: [String] -> String
+decodeModified y = foldr (++) [] ( map decodeHelper y)
